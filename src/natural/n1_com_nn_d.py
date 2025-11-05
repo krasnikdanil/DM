6a31ef4afd765624517c7fefaf4abb0f4200c7e5
= <ninjagames315@gmail.com>
2025-11-05 22:24:23 +0300

    Update Natural class and add comparison functionality

--- src/natural/n1_com_nn_d.py
+++ src/natural/n1_com_nn_d.py
@@ -4,10 +4,10 @@ class NaturalComparison(Natural):
     """Дочерний класс для сравнения натуральных чисел"""
 
     def compare(self, other: 'Natural') -> int:
-        """Сравнение натуральных чисел: 2 - если первое больше второго, 0 - если равно, 1 - иначе"""
+        """Сравнение натуральных чисел: 1 - если первое больше второго, 0 - если равно, -1 - иначе"""
         if int(self.value) > int(other.value):
-            return 2
+            return 1
         elif int(self.value) == int(other.value):
             return 0
         else:
-            return 1
+            return -1
--- src/natural/natural.py
+++ src/natural/natural.py
@@ -1,4 +1,3 @@
-from typing import Union
 from src.common.errors import NaturalError
 
 
