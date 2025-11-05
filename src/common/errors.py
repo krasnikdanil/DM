b49ac358b020de5fcbace717eb423de3b19237ad
sEnq <ninjagames315@gmail.com>
2025-11-05 21:35:50 +0300

    Update Natural class and add comparison functionality

---
+++ src/common/errors.py
@@ -0,0 +1,18 @@
+class NaturalError(Exception):
+    """Исключение для ошибок, связанных с натуральными числами"""
+    pass
+
+
+class IntegerError(Exception):
+    """Исключение для ошибок, связанных с целыми числами"""
+    pass
+
+
+class RationalError(Exception):
+    """Исключение для ошибок, связанных с рациональными числами"""
+    pass
+
+
+class PolynomialError(Exception):
+    """Исключение для ошибок, связанных с многочленами"""
+    pass
---
+++ src/natural/n1_com_nn_d.py
@@ -0,0 +1,13 @@
+from src.natural.natural import Natural
+
+class NaturalComparison(Natural):
+    """Дочерний класс для сравнения натуральных чисел"""
+
+    def compare(self, other: 'Natural') -> int:
+        """Сравнение натуральных чисел: 2 - если первое больше второго, 0 - если равно, 1 - иначе"""
+        if int(self.value) > int(other.value):
+            return 2
+        elif int(self.value) == int(other.value):
+            return 0
+        else:
+            return 1
