386e60b56ae19a196e75227e42c3dc7f04190c91
krasnikdanil <krasnikdanil108@gmail.com>
2025-11-08 19:28:05 +0300

    Update n13_gcf_nn_n.py

--- src/natural/n13_gcf_nn_n.py
+++ src/natural/n13_gcf_nn_n.py
@@ -5,11 +5,10 @@ from src.natural.natural import Natural
 class Gcfnnn(Natural):
     def gcf_nn_n(self,other: 'Natural') -> str:
         a, b = int(self.value), int(other.value)
-        if a == 0 or b == 0:
-            raise ValueError("")
         while a != 0 and b != 0:
             if a > b:
                 a %= b
             else:
                 b %= a
+
         return str(a + b)
